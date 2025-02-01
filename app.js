e4ec3e2e6485f6391eead0b3a5c3f9bdf3bab54f
nwisnie <nwisnie@purdue.edu>
2025-01-31 19:11:50 -0500

    updated appearance

---
+++ app.js
@@ -0,0 +1,58 @@
+// basecode from briancodex: https://github.com/briancodex/html-css-js-website-smooth-scroll/blob/master/index.html
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
+
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const aboutMenu = document.querySelector('#about-page');
+    const projectsMenu = document.querySelector('#projects-page');
+    let scrollPos = window.scrollY;
+
+    if (window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add('highlight');
+        // aboutMenu.classList.remove('highlight');
+        projectsMenu.classList.remove('highlight');
+        return;
+    } else if (window.innerWidth > 960 && scrollPos < 2000) {
+        // aboutMenu.classList.add('highlight');
+        projectsMenu.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
+        // projectsMenu.classList.remove('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;
+    } else if (window.innerWidth > 960 && scrollPos < 2400) {
+        // projectsMenu.classList.add('highlight');
+        aboutMenu.classList.add('highlight');
+        // aboutMenu.classList.remove('highlight');
+        projectsMenu.classList.remove('highlight');
+        return;
+    }
+
+    if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+        elem.classList.remove('highlight');
+    }
+};
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
+
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if (window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
